68b0fadbde48e7dcdc8b257ca02de10d1df46dff
harshitm98 <harshitmah98@gmail.com>
2017-07-30 02:03:32 +0530

    Added setOnClickListener

--- app/src/main/java/com/example/android/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/example/android/qrcodescanner/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.android.qrcodescanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scanButton;
+    private TextView textView;
+    private IntentIntegrator qrscan;
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanButton = (Button)findViewById(R.id.scan_button);
+        textView = (TextView)findViewById(R.id.display_text);
+
+        qrscan = new IntentIntegrator(this);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                qrscan.initiateScan();
+            }
+        });
+
     }
 }
